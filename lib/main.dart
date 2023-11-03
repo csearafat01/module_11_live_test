7a20a46603c5bfdfc56c06eea01cfca79543efd9
csearafat01 <54732088+csearafat01@users.noreply.github.com>
2023-11-03 22:19:53 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CrudApp());
+}
+
+class CrudApp extends StatelessWidget {
+  const CrudApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Module 11 Live Test',
+    );
+  }
+}
